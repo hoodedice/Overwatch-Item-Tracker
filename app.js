c6ad7ef49062d5290b9f14b50201b5a681ee0e33
hoodedice <hoodedice@gmail.com>
2020-10-28 22:20:25 +0400

    typo

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.json({limi: "5mb"}));
+app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
